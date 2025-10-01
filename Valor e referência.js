338ef3629ee0a8056d63e2512c882695e821c7eb
Laura Nunes <lauracostanunes2@gmail.com>
2025-09-30 22:08:17 -0300

    Add files via upload

---
+++ Valor e referência.js
@@ -0,0 +1,17 @@
+function alterarNumero(x) {
+    x += 100
+}
+
+function alterarVetor(v) {
+    v[0] = 100 // Altera variável vetor do main()
+}
+
+function main() {
+    let valor = 10, vetor = [10, 20, 30]
+    alterarNumero(valor)
+    console.log(`Valor: ${valor}`)
+    // Passagem de parâmetros por referência
+    alterarVetor(vetor)
+    console.log(`Vetor: ${vetor}`)
+}
+main()
