aade89aec0bb133ef50c05f314f55ff4a4fe8d70
Laura Nunes <lauracostanunes2@gmail.com>
2025-10-01 20:33:13 -0300

    Add files via upload

---
+++ Processamento de nomes.js
@@ -0,0 +1,31 @@
+function carregarCidades(lista) {
+    return ["São Paulo", "Campinas", "Santos", "Sorocaba", "Caraguatatuba"]
+}
+
+function padronizarNomes(lista) {
+    for (let i = 0; i < lista.length; i++) {
+        lista[i] = lista[i].toUpperCase();
+    }
+}
+
+function marcarInterior(lista) {
+    for (let i = 0; i < lista.length; i++) {
+        if (lista[i][0] == 'C') {
+            lista[i] = `INTERIOR - ${lista[i]}`
+        }
+    }
+}
+
+function exibirCidades(lista) {
+    console.log(lista)
+}
+
+function main () {
+    let nomes = carregarCidades();
+    console.log(nomes)
+    padronizarNomes(nomes)
+    console.log(nomes)
+    marcarInterior(nomes)
+    exibirCidades(nomes)
+}
+main ()
